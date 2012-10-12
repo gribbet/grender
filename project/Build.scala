ae795bfa9f543dcf02f4925def93f2cdb47c0b8f
graham <graham.gibbons@gmail.com>
2012-10-12 06:25:48 -0700

    Initial commit

---
+++ project/Build.scala
@@ -0,0 +1,33 @@
+import sbt._
+import Keys._
+
+object GrenderBuild extends Build {
+  lazy val standardSettings = Defaults.defaultSettings ++ Seq(
+    organization := "org.gribbet",
+    version := "0.1",
+    scalaVersion := "2.9.2",
+    scalacOptions ++= Seq("-deprecation"),
+    resolvers ++= Seq(
+      "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"))
+
+  lazy val grender = Project(
+    id = "grender",
+    base = file("."),
+    settings = standardSettings,
+    aggregate = Seq(core, example)) dependsOn (core, example)
+
+  lazy val core = Project(
+    id = "core",
+    base = file("core"),
+    settings = standardSettings)
+
+  lazy val selector = Project(
+    id = "selector",
+    base = file("selector"),
+    settings = standardSettings)
+    
+  lazy val example = Project(
+    id = "example",
+    base = file("example"),
+    settings = standardSettings) dependsOn ("core")
+}
